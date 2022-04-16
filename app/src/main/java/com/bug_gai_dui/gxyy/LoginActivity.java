89e89c3dea6f473e33375ba20bf736266539ad7f
MoonKuun <zhuxukun@tongji.edu.cn>
2022-04-17 01:11:52 +0800

    登录界面通信 但后端接口还无 需要前后端调试

--- app/src/main/java/com/bug_gai_dui/gxyy/LoginActivity.java
+++ app/src/main/java/com/bug_gai_dui/gxyy/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.bug_gai_dui.gxyy;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.text.Editable;
 import android.view.View;
@@ -8,37 +9,92 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText name;
-    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        name = findViewById(R.id.account_box);
-        password = findViewById(R.id.password_box);
-    }
-    public void login(View v){
-        //这个就是 接受数据库信息，进行判断  json文件读取 自己整
-        //登陆账号的密码
-        String Usename="admin";
-        String Upassword="12345";
-        //创建两个String类，储存从输入文本框获取到的内容
-        String user = name.getText().toString().trim();
-        String pwd = password.getText().toString().trim();
-        if(user.equals(Usename)& pwd.equals(Upassword)){
-            Toast.makeText(this,"Correct",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this,"Wrong",Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void reg(View v){
-        //这个就是 接受数据库信息，进行判断  json文件读取 自己整
-        //登陆账号的密码
-        Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
-        startActivity(intent);
+
+        // 点击“登录”按钮
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+        // 点击“注册”按钮跳转到注册界面
+        Button registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    protected void login(){
+        EditText account_box = findViewById(R.id.account_box);
+        EditText password_box = findViewById(R.id.password_box);
+        String username = account_box.getText().toString().trim();
+        String passwd = password_box.getText().toString().trim();
+        // 检查是否为空
+        if(username.length() == 0)
+        {
+            Toast.makeText(this,"请输入用户名",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(passwd.length() == 0)
+        {
+            Toast.makeText(this,"请输入密码",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    MediaType JSON = MediaType.parse("application/json;charset=utf-8");
+                    //手机发的json内容
+                    JSONObject json = new JSONObject();
+                    json.put("username", username);
+                    json.put("passwd", passwd);
+
+                    OkHttpClient okHttpClient = new OkHttpClient();
+                    //创建一个RequestBody
+                    RequestBody requestBody = RequestBody.create(String.valueOf(json), JSON);
+                    //创建一个请求对象
+                    Request request = new Request.Builder()
+                            .url("http://www.baidu.com") //后端接口
+                            .post(requestBody)
+                            .build();
+
+                    Response response = null;
+                    response = okHttpClient.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        // 暂时假设服务器的回复是一个json，有个result值表示密码正确or错误
+                        json = new JSONObject(response.body().string());
+                        if(json.getBoolean("result")) {
+                            Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT);
+                        } else {
+                            Toast.makeText(LoginActivity.this,"密码错误",Toast.LENGTH_SHORT);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
 }
